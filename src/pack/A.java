d2b857da0c8fd4ca624ce887e73e83fbd92f327e
VuDaiDuong-c0220h1 <61004399+VuDaiDuong-c0220h1@users.noreply.github.com>
2020-04-05 13:36:14 +0700

    Add files via upload

---
+++ src/pack/A.java
@@ -0,0 +1,6 @@
+package pack;
+
+public class A {
+    private int data = 40;
+    protected void msg(){System.out.println("Hello");}
+}
---
+++ src/pack/Simple.java
@@ -0,0 +1,14 @@
+package pack;
+
+import pack.A;
+
+public class Simple {
+    public static void main(String args[]){
+
+        A obj=new A();
+
+        System.out.println(obj.data);//Compile Time Error
+
+        obj.msg();//Compile Time Error
+    }
+}
